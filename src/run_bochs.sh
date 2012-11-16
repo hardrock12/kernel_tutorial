760ee7b8763b601a9baa1ad174854adb4706aced
Yves Baumes <ybaumes@gmail.com>
2012-11-16 19:49:27 +0100

    Finish section two: Genesis

--- src/run_bochs.sh
+++ src/run_bochs.sh
@@ -3,6 +3,11 @@
 # run_bochs.sh
 # mounts the correct loopback device, runs bochs, then unmounts.
 
-hdiutil attach floppy.img -mountpoint mnt -nobrowse
+# Since the "floppy.img" file is a custom one (see the Makefile which
+#create it as the concatenation of stage1, stage2, pad and kernel
+#files), we don't mount it. actually we can't. hditutil does not
+#recognize the format, complains about it and fails. As a result,
+#hdituils lines are commented here. hdiutil attach floppy.img
+#-mountpoint mnt -nobrowse -readwrite
 bochs -f bochsrc.txt
-hdiutil detach mnt
+#hdiutil detach mnt
