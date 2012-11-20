c2f7418c4f074c2e3ea1a7ad1b91c7f4615b7d0a
Yves Baumes <ybaumes@gmail.com>
2012-11-20 21:36:29 +0100

    Chapter five. Implements irq handling bases.

--- src/descriptor_tables.c
+++ src/descriptor_tables.c
@@ -61,6 +61,18 @@ static void init_idt()
 
   memset(&idt_entries,0,sizeof(idt_entry_t)*256);
 
+  // Remap the irq table.
+  outb(0x20, 0x11);
+  outb(0xA0, 0x11);
+  outb(0x21, 0x20);
+  outb(0xA1, 0x28);
+  outb(0x21, 0x04);
+  outb(0xA1, 0x02);
+  outb(0x21, 0x01);
+  outb(0xA1, 0x01);
+  outb(0x21, 0x0);
+  outb(0xA1, 0x0);
+
     /* (dotimes (cpt 32) */
     /*  (insert */
     /*   (format "  idt_set_gate(%d, (u32int)isr%d, 0x08, 0x8E);\n" cpt cpt))) */
@@ -97,6 +109,26 @@ static void init_idt()
   idt_set_gate(30, (u32int)isr30, 0x08, 0x8E);
   idt_set_gate(31, (u32int)isr31, 0x08, 0x8E);
 
+    /* (dotimes (cpt 16) */
+    /*  (insert */
+    /*   (format "  idt_set_gate(%d, (u32int)irq%d, 0x08, 0x8E);\n" (+ cpt 32) cpt)))  */
+  idt_set_gate(32, (u32int)irq0, 0x08, 0x8E);
+  idt_set_gate(33, (u32int)irq1, 0x08, 0x8E);
+  idt_set_gate(34, (u32int)irq2, 0x08, 0x8E);
+  idt_set_gate(35, (u32int)irq3, 0x08, 0x8E);
+  idt_set_gate(36, (u32int)irq4, 0x08, 0x8E);
+  idt_set_gate(37, (u32int)irq5, 0x08, 0x8E);
+  idt_set_gate(38, (u32int)irq6, 0x08, 0x8E);
+  idt_set_gate(39, (u32int)irq7, 0x08, 0x8E);
+  idt_set_gate(40, (u32int)irq8, 0x08, 0x8E);
+  idt_set_gate(41, (u32int)irq9, 0x08, 0x8E);
+  idt_set_gate(42, (u32int)irq10, 0x08, 0x8E);
+  idt_set_gate(43, (u32int)irq11, 0x08, 0x8E);
+  idt_set_gate(44, (u32int)irq12, 0x08, 0x8E);
+  idt_set_gate(45, (u32int)irq13, 0x08, 0x8E);
+  idt_set_gate(46, (u32int)irq14, 0x08, 0x8E);
+  idt_set_gate(47, (u32int)irq15, 0x08, 0x8E);
+
   idt_flush((u32int)&idt_ptr);
 }
 
@@ -123,6 +155,6 @@ static void idt_set_gate(u8int num, u32int base, u16int sel, u8int flags)
    idt_entries[num].always0 = 0;
    // We must uncomment the OR below when we get to using user-mode.
    // It sets the interrupt gate's privilege level to 3.
-   idt_entries[num].flags   = flags /* | 0x60 */;
+   idt_entries[num].flags   = flags /* | 0x60*/;
 }
 
